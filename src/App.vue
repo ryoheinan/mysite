43ff0e4f5dabfbf89f2e0f25d73c745d32ca9135
aleeca <49315610+ryoheinan@users.noreply.github.com>
2021-05-04 16:00:52 +0900

    Footer.vueの作成

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,30 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <Footer />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+  import { defineComponent } from 'vue'
+  import HelloWorld from './components/HelloWorld.vue'
+  import Footer from './components/Footer.vue'
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-})
+  export default defineComponent({
+    name: 'App',
+    components: {
+      HelloWorld,
+      Footer,
+    },
+  })
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer>
+    <p>&copy; {{ year }}, Ryohei Nagasawa All Rights Reserved.</p>
+  </footer>
+</template>
+
+<script lang="ts">
+  import { defineComponent, ref, computed } from 'vue'
+  export default defineComponent({
+    name: 'HelloWorld',
+    setup: () => {
+      const nowYear = computed(() => new Date().getFullYear())
+      const year = ref(nowYear)
+      return { year }
+    },
+  })
+</script>
+
+<style lang="scss" scoped></style>
