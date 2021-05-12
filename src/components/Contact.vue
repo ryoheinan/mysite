148acc3909783dc911ea651f292f7a95bc7407dd
aleeca <49315610+ryoheinan@users.noreply.github.com>
2021-05-12 18:33:12 +0900

    Add Contact.vue

---
+++ src/components/Contact.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <Blankslate
+      btn-text="Contact me"
+      btn-url="#"
+      description="This contact form use Google Form."
+    />
+  </section>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import Blankslate from './Blankslate.vue'
+  export default defineComponent({
+    name: 'Contact',
+    components: {
+      Blankslate,
+    },
+  })
+</script>
