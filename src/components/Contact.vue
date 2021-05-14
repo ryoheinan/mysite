37620d779d768145a76c43fdcf52aad3dacd7d23
aleeca <49315610+ryoheinan@users.noreply.github.com>
2021-05-15 00:44:11 +0900

    Fix contact description

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -3,7 +3,7 @@
     <Blankslate
       btn-text="Contact me"
       btn-url="https://forms.gle/NC4t6RMXaKtE2voe6"
-      description="This contact form use Google Forms"
+      description="This contact form uses Google Forms"
     />
   </section>
 </template>
