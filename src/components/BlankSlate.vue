0091a3a099b5bbb835a8b91ded39d6b01583e114
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2021-11-18 19:45:58 +0900

    Create BlankSlate.vue

---
+++ src/components/BlankSlate.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+  interface Props {
+    description: string
+    btnText?: string
+    btnUrl?: string
+  }
+
+  defineProps<Props>()
+</script>
+
+<template>
+  <div class="blankslate">
+    <a
+      v-if="btnText"
+      :href="btnUrl"
+      class="btn btn-large btn-primary h4 mb-1 py-3"
+      role="button"
+      target="_blank"
+      rel="noopener noreferrer nofollow"
+      >{{ btnText }}</a
+    >
+    <p>{{ description }}</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  @import '@primer/css/blankslate/index.scss';
+  @import '@primer/css/marketing/index.scss';
+</style>
