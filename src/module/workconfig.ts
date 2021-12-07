ba9e35855b81d526a0d68867b61e6d696db97c07
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2021-12-08 00:45:21 +0900

    Create image interface

--- src/module/workconfig.ts
+++ src/module/workconfig.ts
@@ -3,6 +3,8 @@ export interface Work {
   description?: string
   technology: string
   category: 'School' | 'Hobby' | 'Work'
+  year: string
+  image?: Image
   urls?: Array<Urls>
 }
 
@@ -11,12 +13,19 @@ interface Urls {
   url: string
 }
 
+interface Image {
+  src: string
+  alt: string
+  srcset: string
+}
+
 export const workData: Work[] = [
   {
     title: 'Personal website',
     description: 'This website!!',
     technology: 'Vue 3, TypeScript',
     category: 'Hobby',
+    year: '2021 summer ~ now',
     urls: [
       {
         text: 'Source code',
@@ -30,6 +39,12 @@ export const workData: Work[] = [
       'My friend, Taito and I joined hackathon in the spring of 2021. We developed this web app for it.',
     technology: 'Nuxt 2, LIFF (LINE Front-end Framework), Django',
     category: 'Hobby',
+    year: '2021 spring',
+    image: {
+      src: 'morphomusic.jpg',
+      alt: 'an image of morphomusic',
+      srcset: 'morphomusic.webp',
+    },
     urls: [
       {
         text: 'Source code (client)',
@@ -43,9 +58,16 @@ export const workData: Work[] = [
   },
   {
     title: 'Game : The Tortoise and the Hare',
-    description: 'A famous story, "The Tortoise and the Hare"-themed PC game.',
+    description:
+      'A famous story, "The Tortoise and the Hare (a.k.a. Usagitokame)"-themed PC game.',
     technology: 'Unity, C#',
     category: 'School',
+    year: '2020 winter',
+    image: {
+      src: 'usakame.png',
+      alt: 'an image of "The Tortoise and the Hare"-themed PC game',
+      srcset: 'usakame.webp',
+    },
     urls: [
       {
         text: 'Presentation slides (Japanese)',
@@ -58,22 +80,11 @@ export const workData: Work[] = [
     ],
   },
   {
-    title: 'Sugawara Soroban website',
-    description: 'My first job.',
-    technology: 'Nuxt 2',
-    category: 'Work',
-    urls: [
-      {
-        text: 'Website',
-        url: 'https://sugawara-soroban.com/',
-      },
-    ],
-  },
-  {
     title: 'UniWeather',
     description: 'Universal design weather forecast site.',
     technology: 'Vue 2',
     category: 'School',
+    year: '2020 summer',
     /*
     urls: [
       {
@@ -82,5 +93,23 @@ export const workData: Work[] = [
       },
     ],
     */
+  },
+  {
+    title: 'Sugawara Soroban website',
+    description: 'My first job.',
+    technology: 'Nuxt 2',
+    category: 'Work',
+    year: '2019 winter ~ now',
+    image: {
+      src: 'soroban.png',
+      alt: 'an image of Sugawara Soroban website',
+      srcset: 'soroban.webp',
+    },
+    urls: [
+      {
+        text: 'Website',
+        url: 'https://sugawara-soroban.com/',
+      },
+    ],
   },
 ]
