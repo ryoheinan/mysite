2e75ee1c6135457f11f39ee5c3c46fe335c8098c
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2021-12-19 11:42:01 +0900

    Create Skill.vue

---
+++ src/components/Skill.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+  import { computed } from 'vue'
+  import { skillData } from '../module/skillconfig'
+  import SkillCard from './SkillCard.vue'
+  const skillDataset = computed(() => skillData)
+</script>
+<template>
+  <section>
+    <h2>About my skills...</h2>
+    <div v-for="skill in skillDataset" :key="skill.title" class="py-1">
+      <SkillCard :title="skill.title" :bg-color="skill.bgColor">
+        <div>
+          <span
+            v-for="item in skill.items"
+            :key="item.title"
+            class="mx-1 nobreak"
+            >{{ item.title }}</span
+          >
+        </div>
+      </SkillCard>
+    </div>
+  </section>
+</template>
