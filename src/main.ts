3a399567a090db2961c19492c17d79149cb693fe
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2022-01-18 21:29:37 +0900

    Make it possible to set language via query param

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,25 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import { createPinia } from 'pinia'
+import { useSettingsStore } from './stores/settings'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [],
+})
+
+router.beforeEach((to) => {
+  // set lang setting via query param
+  const lang = to.query.lang
+  if (lang === 'en') {
+    const settings = useSettingsStore()
+    settings.lang = lang
+  }
+})
+
+const app = createApp(App)
+app.use(router)
+app.use(createPinia())
+app.mount('#app')
