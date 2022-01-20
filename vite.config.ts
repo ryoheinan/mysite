8c8575331e7f5559f3c847b30c96979b3c13d9b1
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2022-01-20 17:35:34 +0900

    Use vite-plugin-fonts to load fonts instead of CDN

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import ViteFonts from 'vite-plugin-fonts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    ViteFonts({
+      google: {
+        families: [
+          {
+            name: 'Noto Sans JP',
+            styles: 'wght@400;500',
+          },
+        ],
+      },
+    }),
+  ],
 })
