e31711e79b79978f17bee632959dbc3581c2733b
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2022-01-22 14:45:03 +0900

    Fix the color of globe icon when light theme mode

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -97,6 +97,9 @@
     .globe-icon {
       margin-top: 4.5px;
       svg {
+        @media (prefers-color-scheme: light) {
+          fill: #57606a;
+        }
         @media (prefers-color-scheme: dark) {
           fill: #8b949e;
         }
