e9a0e3e1223a0c0d4627343b8cf4028bdab37292
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2022-02-12 00:04:42 +0900

    Create alert component

---
+++ src/components/Alert.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+  interface Props {
+    msg: string
+    link?: {
+      title: string
+      url: string
+    }
+  }
+  const props = defineProps<Props>()
+</script>
+
+<template>
+  <div class="flash">
+    {{ props.msg }}
+    <a
+      v-if="props.link"
+      class="btn btn-primary btn-sm flash-action"
+      :href="props.link.url"
+      target="_blank"
+      rel="noopener noreferrer"
+      >{{ props.link.title }}</a
+    >
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  @import '@primer/css/alerts/index.scss';
+</style>
