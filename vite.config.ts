41a1c4b42ca3f87d9286cc57d90fec628d375d00
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2022-02-22 21:46:33 +0900

    Set `preprocessorOptions` Resolves #59

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import { ViteWebfontDownload } from 'vite-plugin-webfont-dl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      sass: { charset: false },
+      scss: { charset: false },
+    },
+  },
   plugins: [
     vue(),
     ViteWebfontDownload([
