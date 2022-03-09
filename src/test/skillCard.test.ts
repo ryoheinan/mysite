c2f9323b876ce6dc7139e76227b605fb48f7fd6d
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2022-03-09 17:11:17 +0900

    Create test for skill card component

---
+++ src/test/skillCard.test.ts
@@ -0,0 +1,29 @@
+import { mount } from '@vue/test-utils'
+import SkillCard from '../components/SkillCard.vue'
+import { describe, expect, it } from 'vitest'
+
+describe('SkillCard component', () => {
+  expect(SkillCard).toBeTruthy()
+
+  const wrapper = mount(SkillCard, {
+    props: {
+      title: 'Excellent',
+      styleName: 'skill-excellent',
+      items: [{ title: 'item1' }, { title: 'item2' }, { title: 'item3' }],
+    },
+  })
+
+  it('msgが正しく設定されているか', () => {
+    expect(wrapper.find('h3').text()).toContain('Excellent')
+  })
+  it('itemが正しく設定されているか', () => {
+    expect(wrapper.find('[data-test="items"]').exists()).toBeTruthy()
+    expect(wrapper.find('[data-test="items"]').text()).toContain('item1')
+    expect(wrapper.find('[data-test="items"]').text()).toContain('item2')
+    expect(wrapper.find('[data-test="items"]').text()).toContain('item3')
+    expect(wrapper.findAll('[data-test="item"]')).toHaveLength(3)
+  })
+  it('Snapshot', () => {
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
