0c5740072c4fa9e158906958bf588f0aa87079af
Ryohei Nagasawa <49315610+ryoheinan@users.noreply.github.com>
2022-03-09 20:28:47 +0900

    Remove `vitest.config.ts`

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 import { ViteWebfontDownload } from 'vite-plugin-webfont-dl'
 
@@ -16,4 +16,8 @@ export default defineConfig({
       'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap',
     ]),
   ],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+  },
 })
